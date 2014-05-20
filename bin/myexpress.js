1323d054fbfe07c5f99ccb956574f5bf17c2fbb0
wild-flame <wildflame@126.com>
2014-05-20 10:15:31 +0800

    [Finish] Lesson 3

--- bin/myexpress.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-var express = require('myexpress');
-console.log(express)
-
-var app = express();
-var http = require("http");
-var server = http.createServer(app);
-server.listen(4000)
